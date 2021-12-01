4774ebdb54f54590e45213e29d908318f9e02b60
adityaharsh2001 <geekyadi118@gmail.com>
2021-12-01 23:05:31 +0530

    home design

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -39,17 +39,15 @@ function Header() {
     return (
         <header>
             <div className="logo">
-                <h1><Link to="/">DevAT✮Shop</Link></h1>
+                <h1><Link to="/">ContestApp</Link></h1>
             </div>
 
             <ul style={transForm}>
-                <li><Link to="/"><i className="fas fa-shopping-cart"></i> Cart</Link></li>
                 {
                     isLogged
                     ? userLink()
                     :<li><Link to="/login"><i className="fas fa-user"></i> Sign in</Link></li>
                 }
-                
             </ul>
         </header>
     )
